abcde2776441181850870b876875b7e995c14a9b
Surya..!!! <surya-zs1@proton.me>
2026-04-22 17:35:54 +0530

    Update mediainfo.py
    new info

--- bot/processes/mediainfo.py
+++ bot/processes/mediainfo.py
@@ -30,7 +30,7 @@ class MediaInfoProcess(BaseProcess):
         await self.set_media_message()
         await self.input_message.edit_message_text(ms.PROCESSING_REQUEST)
         try:
-            if self.media_message.empty:
+            if self.media_message is None or getattr(self.media_message, 'empty', False) or getattr(self.media_message, 'date', None) is None:
                 raise MediaInfoProcessFailure(
                     for_user=ms.MEDIA_MESSAGE_DELETED,
                     for_admin=ms.MEDIA_MESSAGE_DELETED,
