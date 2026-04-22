1d3414d4d897a2d4d4468f8058ce8af534c3c7ea
Surya..!!! <surya-zs1@proton.me>
2026-04-22 17:28:38 +0530

    Update manual_screenshot_1.py

--- bot/plugins/manual_screenshot_1.py
+++ bot/plugins/manual_screenshot_1.py
@@ -17,6 +17,6 @@ async def _(c, m):
         "\nThis will generate screenshots at 0, 10, 40, 60, and 120 seconds. \n\n"
         "1. The list can have a maximum of 10 valid positions.\n"
         "2. The position has to be greater than or equal to 0, or less than the video length in order to be valid.",
-        reply_to_message_id=m.message.reply_to_message.message_id,
+        reply_to_message_id=m.message.reply_to_message.id,
         reply_markup=ForceReply(),
     )
